04ebf8a87120580c5252cd71877db192dfaccc31
KarlKardon <collincunningham@gmail.com>
2023-10-21 18:12:15 -0500

    half of react tutorial done

---
+++ hack-tx/src/Navbar.js
@@ -0,0 +1,13 @@
+
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The fuck???</h1>
+            <div className="links">
+                <a href="youtube.com">Home</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
